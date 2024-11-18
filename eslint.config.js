4926a2929b5d802edf1aa0acf17b0e5f4c9a8428
Aresguerre <bacem.b.achour@gmail.com>
2024-11-18 13:31:27 +0100

    initial project setup

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptEslintParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: ['node_modules/**/*'],
+  },
+  {
+    files: ['src/**/*.{ts,js}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: typescriptEslintParser,
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslintPlugin,
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      '@typescript-eslint/no-unused-vars': ['error'],
+    },
+  },
+];
