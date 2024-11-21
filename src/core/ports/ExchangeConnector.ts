9cf8032c11053444512a290bbe5673abc46f66d1
Aresguerre <bacem.b.achour@gmail.com>
2024-11-21 15:49:38 +0100

    makes isConnected async

--- src/core/ports/ExchangeConnector.ts
+++ src/core/ports/ExchangeConnector.ts
@@ -4,4 +4,7 @@ import { Pair } from '../domain/Pair';
 export interface ExchangeConnector {
   fetchTopOfBook(pair: Pair): Promise<TopOfBook | null>;
   getName(): string;
+  connect(): Promise<void>;
+  disconnect(): Promise<void>;
+  isConnected(): Promise<boolean>;
 }
